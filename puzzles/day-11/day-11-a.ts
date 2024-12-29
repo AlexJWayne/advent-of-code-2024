ee4d2ecbc0e7ebb202ba2b34dd900eeac725e08b
AlexJWayne <alex.wayne@solvenergy.com>
2024-12-28 18:32:04 -0800

    cleanup

--- puzzles/day-11/day-11-a.ts
+++ puzzles/day-11/day-11-a.ts
@@ -22,14 +22,13 @@ export function day11a([line]: string[]): number {
 function blink(value: number, iterations: number): number {
   const cached = memo[value]?.[iterations];
   if (cached !== undefined) return cached;
+
   if (!memo[value]) memo[value] = {};
 
   if (iterations === 0) return 1;
 
   if (value === 0) {
-    const result = blink(1, iterations - 1);
-    memo[value][iterations] = result;
-    return result;
+    return cache(value, iterations, blink(1, iterations - 1));
   }
 
   const digits = Math.floor(Math.log10(value)) + 1;
@@ -38,12 +37,17 @@ function blink(value: number, iterations: number): number {
     const right = value % halfPower;
     const left = Math.floor(value / halfPower);
 
-    const result = blink(left, iterations - 1) + blink(right, iterations - 1);
-    memo[value][iterations] = result;
-    return result;
+    return cache(
+      value,
+      iterations,
+      blink(left, iterations - 1) + blink(right, iterations - 1),
+    );
   }
 
-  const result = blink(value * 2024, iterations - 1);
+  return cache(value, iterations, blink(value * 2024, iterations - 1));
+}
+
+function cache(value: number, iterations: number, result: number): number {
   memo[value][iterations] = result;
   return result;
 }
