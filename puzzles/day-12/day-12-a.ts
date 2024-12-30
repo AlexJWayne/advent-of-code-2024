60dc045daac83dabedd6086abbcc704816450afa
AlexJWayne <alex.wayne@solvenergy.com>
2024-12-29 22:44:51 -0800

    day 12b progress

--- puzzles/day-12/day-12-a.ts
+++ puzzles/day-12/day-12-a.ts
@@ -1,84 +1,125 @@
-import Victor from 'victor';
-import { Grid, toKey } from '../lib/grid.ts';
-import { Point } from '../lib/vector.ts';
-import chalk from 'chalk/index.js';
+import Victor from 'victor'
+import { Grid } from '../lib/grid.ts'
+import { Point } from '../lib/vector.ts'
 
-const adjacent = [
+export const adjacent = [
   new Victor(1, 0),
   new Victor(0, 1),
   new Victor(-1, 0),
   new Victor(0, -1),
-];
+]
 
-let grid: Grid<{ v: string; id: number }>;
-
-let nextRegionId = 1;
-const regions: Map<number, string> = new Map();
-const regionsToPoints: Map<number, Set<Point>> = new Map();
+export type Ctx = {
+  grid: Grid<{ v: string; id: number }>
+  nextRegionId: number
+  regions: Map<number, string>
+  regionsToPoints: Map<number, Set<Point>>
+}
 
 export function day12a(data: string[]): number {
-  grid = Grid.fromLines(data, { v: '.', id: 0 }, (char) => ({ v: char }));
+  const ctx: Ctx = createContext(data)
 
-  grid.print(({ v, id }) => v);
+  // grid.print(({ v, id }) => v);
+  parseRegions(ctx)
+  // grid.print(({ v, id }) => `${id}\t` || '\t');
 
-  grid.forEach((cell, point) => {
-    const adjRegions = findAdjacentRegions(point);
+  return getPrice(ctx)
+}
+
+export function createContext(data: string[]): Ctx {
+  return {
+    grid: Grid.fromLines(data, { v: '.', id: 0 }, (char) => ({ v: char })),
+    nextRegionId: 1,
+    regions: new Map(),
+    regionsToPoints: new Map(),
+  }
+}
+
+export function parseRegions(ctx: Ctx) {
+  ctx.grid.forEach((cell, point) => {
+    const adjRegions = findAdjacentRegions(ctx, point)
 
     if (adjRegions.size === 0) {
-      const id = createRegion(cell.v);
-      addToRegion(point, id);
+      const id = createRegion(ctx, cell.v)
+      addToRegion(ctx, point, id)
     } else {
-      const id = adjRegions.values().next().value;
-      addToRegion(point, id);
+      const id = adjRegions.values().next().value
+      addToRegion(ctx, point, id)
 
-      if (adjRegions.size > 1) mergeRegions(adjRegions);
+      if (adjRegions.size > 1) mergeRegions(ctx, adjRegions)
     }
-  });
-
-  grid.print(({ v, id }) => `${id}\t` || '\t');
-
-  //const price = [...regions.keys()].map()
-
-  return 0;
+  })
 }
 
-function findAdjacentRegions(point: Point): Set<number> {
-  const { v } = grid.get(point);
-  const ids = new Set<number>();
+function findAdjacentRegions({ grid }: Ctx, point: Point): Set<number> {
+  const { v } = grid.get(point)
+  const ids = new Set<number>()
   for (const adj of adjacent) {
-    const adjCell = grid.get({ x: point.x + adj.x, y: point.y + adj.y });
-    if (adjCell?.id && v === adjCell.v) ids.add(adjCell.id);
+    const adjCell = grid.get({ x: point.x + adj.x, y: point.y + adj.y })
+    if (adjCell?.id && v === adjCell.v) ids.add(adjCell.id)
   }
-  return ids;
+  return ids
 }
 
-function createRegion(v: string): number {
-  const id = nextRegionId++;
-  regions.set(id, v);
-  return id;
+function createRegion(ctx: Ctx, v: string): number {
+  const id = ctx.nextRegionId++
+  ctx.regions.set(id, v)
+  return id
 }
 
-function addToRegion(point: Point, id: number) {
-  grid.set(point, { id });
+function addToRegion({ grid, regionsToPoints }: Ctx, point: Point, id: number) {
+  grid.set(point, { id })
 
-  if (!regionsToPoints.get(id)) regionsToPoints.set(id, new Set());
-  regionsToPoints.get(id).add(point);
+  if (!regionsToPoints.get(id)) regionsToPoints.set(id, new Set())
+  regionsToPoints.get(id).add(point)
 }
 
-function mergeRegions(ids: Iterable<number>) {
-  const id = nextRegionId++;
+function mergeRegions(ctx: Ctx, ids: Iterable<number>) {
+  const id = ctx.nextRegionId++
 
   for (const prevId of ids) {
-    const v = regions.get(prevId);
-    regions.set(id, v);
-    regions.delete(prevId);
+    const v = ctx.regions.get(prevId)
+    ctx.regions.set(id, v)
+    ctx.regions.delete(prevId)
 
-    const points = regionsToPoints.get(prevId);
-    regionsToPoints.set(id, points);
-    regionsToPoints.delete(prevId);
+    const points = ctx.regionsToPoints.get(prevId)
+    ctx.regionsToPoints.set(
+      id,
+      (ctx.regionsToPoints.get(id) ?? new Set()).union(points),
+    )
+    ctx.regionsToPoints.delete(prevId)
 
     for (const point of points) {
-      grid.set(point, { id });
+      ctx.grid.set(point, { id })
     }
   }
+}
+
+function findPerimeterAtPoint({ grid }: Ctx, point: Point): number {
+  const { v } = grid.get(point)
+  const connectedSides = adjacent.filter((adjPoint) => {
+    return (
+      v ===
+      grid.get({
+        x: point.x + adjPoint.x,
+        y: point.y + adjPoint.y,
+      })?.v
+    )
+  }).length
+  return 4 - connectedSides
+}
+
+function getPrice(ctx: Ctx): number {
+  return [...ctx.regions.keys()]
+    .map((id) => {
+      const v = ctx.regions.get(id)
+      const points = [...ctx.regionsToPoints.get(id)]
+
+      const perimeter = points
+        .map((point) => findPerimeterAtPoint(ctx, point))
+        .reduce((acc, v) => acc + v, 0)
+
+      return perimeter * points.length
+    })
+    .reduce((acc, v) => acc + v, 0)
 }
--- puzzles/day-12/day-12-b.ts
+++ puzzles/day-12/day-12-b.ts
@@ -1,3 +1,46 @@
+import { Point } from '../lib/vector.ts'
+import { adjacent, createContext, Ctx, parseRegions } from './day-12-a.ts'
+
 export function day12b(data: string[]): number {
-  return 0;
+  const ctx = createContext(data)
+  parseRegions(ctx)
+
+  return 0
+}
+
+function getPerimeterSegments(
+  { grid, regionsToPoints, regions }: Ctx,
+  id: number,
+): [Point, Point][] {
+  const v = regions.get(id)
+  const points = regionsToPoints.get(id)
+  const segments: [Point, Point][] = []
+
+  for (const point of points) {
+    for (const [i, direction] of adjacent.entries()) {
+      const leftDirection = adjacent[i === 0 ? 3 : i - 1]
+      const rightDirection = adjacent[i === 3 ? 0 : i + 1]
+
+      const cell = grid.get({
+        x: point.x + direction.x,
+        y: point.y + direction.y,
+      })
+      const isEdge = v !== cell?.v
+
+      if (isEdge) {
+        segments.push([
+          {
+            x: point.x + Math.max(leftDirection.x, 0),
+            y: point.y + Math.max(leftDirection.y, 0),
+          },
+          {
+            x: point.x + Math.max(rightDirection.x, 0),
+            y: point.y + Math.max(rightDirection.y, 0),
+          },
+        ])
+      }
+    }
+  }
+
+  return segments
 }
